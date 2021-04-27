5b83f8b0e587d78f2e472713ee4f43e9cf26c82a
Jessie-p05 <jxpei05@gmail.com>
2021-04-27 04:52:52 +0000

    use join query to search database

---
+++ 2_queries_joins/2_total_time_for_assignment.sql
@@ -0,0 +1,13 @@
+-- SELECT SUM(assignment_submissions.duration) 
+-- FROM students 
+-- JOIN assignment_submissions on students.id = assignment_submissions.student_id
+-- WHERE students.cohort_id =
+-- (
+--   SELECT id as corhort_id
+--   FROM cohorts
+--   WHERE name = 'FEB12');
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'FEB12';
